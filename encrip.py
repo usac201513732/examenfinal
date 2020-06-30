181f2190a755c005cb9a0b1f9f0bc4ce2622a361
usac201513732 <66382236+usac201513732@users.noreply.github.com>
2020-06-29 21:53:33 -0600

    encriptacion

---
+++ encrip.py
@@ -0,0 +1,18 @@
+'''from Crypto.Cipher import *
+import hashlib
+password = "mypassword".encode()
+key  = hashlib.sha256(password).digest()
+mode  = AES.MODE_CBC
+    #print (key)
+    IV  = 'THIS IS AN iv456'
+
+    def p.message(message)
+    wile len(message)% 16! =0 :
+        message = message + ""
+    return message
+
+  
+cipher = AES.new(key, mod, IV)
+message= "entrada mensaje"
+padded_message = pad_message(message)
+encrypted_message  = cipher.encrypt(padded_message) '''
